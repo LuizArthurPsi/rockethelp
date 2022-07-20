c4c2b2f7d33abcb57388c15e8bea767b708894b9
Luiz Arthur <luizarthurvm@gmail.com>
2022-07-20 20:18:08 -0300

    Add global theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from './src/screens/SignIn';
 
+import { THEME } from './src/styles/theme';
+
 import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <SignIn />
     </NativeBaseProvider>
     
