734b01e0cf74bcb54cf6849094778b3a0cb0a4ee
Luiz Arthur <luizarthurvm@gmail.com>
2022-07-24 16:51:52 -0300

    Add Routes
    React Navigation

--- App.tsx
+++ App.tsx
@@ -8,8 +8,9 @@ import {
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 
-import SignIn from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
